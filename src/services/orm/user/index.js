d1d36a99085617d730114c39da99e61423382836
Jesse Kernaghan <jesse@versett.com>
2018-11-17 14:58:50 -0700

    flow tweaks, scaffodling

---
+++ src/services/orm/user/index.js
@@ -0,0 +1,23 @@
+
+const register = function register() {
+
+}
+
+const login = function login() {
+
+}
+
+const requestResetPassword = function() {
+
+}
+
+const resetPassword = function() {
+
+}
+
+export default {
+  register,
+  login,
+  requestResetPassword,
+  resetPassword
+}
